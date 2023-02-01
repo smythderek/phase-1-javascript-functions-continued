7976a7ef96d5e281f145f78ccbabe4907b6c865f
Derek Smyth <smyth.derek@gmail.com>
2023-02-01 09:14:07 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+        return 'This Saturday, I want to roller-skate!'
+    }
+    else {
+        return `This Saturday, I want to ${activity}!`
+    }
+}
+
+/*
+function mondayWork(activity) {
+    if (activity !== undefined) {
+        return `This Monday, I will ${activity}.`
+    }
+    else {
+        return 'This Monday, I will go to the office.'
+    }
+}
+*/
+
+const mondayWork = function(activity) {
+    if (activity !== undefined) {
+        return `This Monday, I will ${activity}.`
+    }
+    else {
+        return 'This Monday, I will go to the office.'
+    }
+}
+
+function wrapAdjective(flair = '*') {
+    return function(word = 'special') {
+        return `You are ${flair}${word}${flair}!`
+    }
+}
